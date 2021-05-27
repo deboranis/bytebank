0031f6dcf9e8142edbbc67b6034ac276853e78a6
Débora Nisenbaum <debora.nisenbaum@nubank.com.br>
2021-05-27 14:42:45 -0300

    first commit, still finishing module 2

---
+++ lib/screens/contact_form.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:newbytebank/database/app_database.dart';
+import 'package:newbytebank/models/contact.dart';
+
+class ContactForm extends StatefulWidget {
+  @override
+  _ContactFormState createState() => _ContactFormState();
+}
+
+class _ContactFormState extends State<ContactForm> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _accountNumberController =
+      TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('New contact'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(labelText: 'Full name'),
+              style: TextStyle(fontSize: 24),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: TextField(
+                controller: _accountNumberController,
+                decoration: InputDecoration(labelText: 'Account number'),
+                style: TextStyle(fontSize: 24),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: SizedBox(
+                  width: double.maxFinite, //tipo um width 100%
+                  child: ElevatedButton(
+                      onPressed: () {
+                        final String name = _nameController.text;
+                        final int accountNumber =
+                            int.tryParse(_accountNumberController.text);
+                        final Contact newContact = Contact(0, name,
+                            accountNumber); //transformando em um objeto
+                        save(newContact).then(
+                          (id) => Navigator.pop(context),
+                        );
+                      },
+                      child: Text('Create'))),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/contact_list.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:newbytebank/database/app_database.dart';
+import 'package:newbytebank/models/contact.dart';
+import 'package:newbytebank/screens/contact_form.dart';
+
+class ContactList extends StatefulWidget {
+
+  @override
+  _ContactListState createState() => _ContactListState();
+}
+
+class _ContactListState extends State<ContactList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contacts'),
+      ),
+      body: FutureBuilder<List<Contact>>( //basicamente um stateful widget
+        initialData: List(),
+        future: findAll(), //vai buscar todas as ocorrencias de contatos na lista da database, mas sempre dando um delay porque quando inicia a aplicação vai retornar null
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              print('none');
+              break;
+            case ConnectionState.waiting: //quando tá carregando
+              print('waiting');
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    CircularProgressIndicator(),
+                    Text('Loading'),
+                  ],
+                ),
+              );
+              break;
+            case ConnectionState.active: //o snapshot tem um dado disponível, mas nao foi finalizado o Future. por exemplo, dados de um download. pra fazer barrinha de progresso.
+              break;
+            case ConnectionState.done: //quando ja rolou o future
+              final List<Contact> contacts = snapshot.data; //estamos dizendo que a lista será o conteudo que é retornado da Future
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final Contact contact = contacts[index];
+                  return _ContactItem(contact);
+                },
+                itemCount: contacts.length,
+              );
+              break;
+          }
+          return Text('Unknown error'); //nunca vai chegar aqui, pq já cobrimos todos os cenários do switch case. mas precisa ter um retorno de qualquer forma. é sempre bom evitar return null! devolva uma msg genérica de erro
+        },
+      ),
+      //
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => ContactForm(),
+            ),
+          ).then((value) => setState(() {}));
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class _ContactItem extends StatelessWidget {
+  final Contact contact;
+
+  _ContactItem(this.contact);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+          title: Text(
+        contact.name,
+        style: TextStyle(fontSize: 24),
+      ),
+          subtitle: Text(
+        contact.accountNumber.toString(),
+        style: TextStyle(fontSize: 16),
+      ),
+    ));
+  }
+}
